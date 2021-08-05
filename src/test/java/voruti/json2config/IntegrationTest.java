f0a4c612f4d673f11d05076111f00665768adf06
voruti <45130096+voruti@users.noreply.github.com>
2021-08-05 10:39:13 +0200

    Adjust test to Gradle

--- src/test/java/voruti/json2config/IntegrationTest.java
+++ src/test/java/voruti/json2config/IntegrationTest.java
@@ -15,24 +15,20 @@ class IntegrationTest {
     @Test
     void example1() throws IOException, URISyntaxException {
         // arrange:
-        // create "out" directory:
-        Path path = Paths.get("target/test-classes/out/");
-        Files.createDirectories(path);
-
         // load template .items file:
         Path pathTemplate = Paths.get(ClassLoader.getSystemResource("json.items").toURI());
         String template = String.join(System.lineSeparator(), Files.readAllLines(pathTemplate));
 
         // act:
-        Starter.main(new String[]{"-i", "target/test-classes/org.eclipse.smarthome.core.items.Item.json",
-                "-o", "target/test-classes/out/junit.items",
+        Starter.main(new String[]{"-i", "build/resources/test/org.eclipse.smarthome.core.items.Item.json",
+                "-o", "build/tmp/test/junit.items",
                 "-c",
-                "-d", "target/test-classes/out/",
-                "--channel-file", "target/test-classes/org.eclipse.smarthome.core.thing.link.ItemChannelLink.json"});
+                "-d", "build/tmp/test/",
+                "--channel-file", "build/resources/test/org.eclipse.smarthome.core.thing.link.ItemChannelLink.json"});
 
         // assert:
         // load generated .items file:
-        Path pathGenerated = Paths.get(ClassLoader.getSystemResource("out/junit.items").toURI());
+        Path pathGenerated = Paths.get("build/tmp/test/junit.items");
         String generated = String.join(System.lineSeparator(), Files.readAllLines(pathGenerated));
 
         assertEquals(template, generated);
