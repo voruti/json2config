7e83dda746e9b7a44476f8c270c10f99789691ed
voruti <45130096+voruti@users.noreply.github.com>
2021-08-05 10:40:06 +0200

    Ignore whitespace

--- src/test/java/voruti/json2config/IntegrationTest.java
+++ src/test/java/voruti/json2config/IntegrationTest.java
@@ -17,7 +17,8 @@ class IntegrationTest {
         // arrange:
         // load template .items file:
         Path pathTemplate = Paths.get(ClassLoader.getSystemResource("json.items").toURI());
-        String template = String.join(System.lineSeparator(), Files.readAllLines(pathTemplate));
+        String template = String.join(System.lineSeparator(), Files.readAllLines(pathTemplate))
+                .replaceAll("[\\h\\t ]{2,}", " ");
 
         // act:
         Starter.main(new String[]{"-i", "build/resources/test/org.eclipse.smarthome.core.items.Item.json",
@@ -29,7 +30,8 @@ class IntegrationTest {
         // assert:
         // load generated .items file:
         Path pathGenerated = Paths.get("build/tmp/test/junit.items");
-        String generated = String.join(System.lineSeparator(), Files.readAllLines(pathGenerated));
+        String generated = String.join(System.lineSeparator(), Files.readAllLines(pathGenerated))
+                .replaceAll("[\\h\\t ]{2,}", " ");
 
         assertEquals(template, generated);
     }
