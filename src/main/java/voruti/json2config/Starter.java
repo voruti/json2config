dff45b0c21d8ccafaeb272b494e5ad16db6a0c58
voruti <45130096+voruti@users.noreply.github.com>
2021-08-06 13:17:16 +0200

    Add picocli

--- src/main/java/voruti/json2config/Starter.java
+++ src/main/java/voruti/json2config/Starter.java
@@ -1,6 +1,9 @@
 package voruti.json2config;
 
 import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import voruti.json2config.service.ChannelAppender;
 import voruti.json2config.service.Converter;
 import voruti.json2config.service.Converter.Type;
@@ -9,109 +12,52 @@ import voruti.json2config.service.Converter.Type;
  * @author voruti
  */
 @Slf4j
-public class Starter {
+@Command(name = "java -jar json2config.jar", mixinStandardHelpOptions = true, version = "1.4",
+        description = "Converts openHAB Items from JsonDB Storage files.")
+public class Starter implements Runnable {
 
-    private static final String DEFAULT_JSONFILE = "org.eclipse.smarthome.core.items.Item.json";
-    private static final String DEFAULT_CHANNELFILE = "org.eclipse.smarthome.core.thing.link.ItemChannelLink.json";
-    private static final String DEFAULT_OUTFILE = "json.items";
-    private static final String DEFAULT_DIRECTORY = ".";
+    @Option(names = {"--no-items", "-n"},
+            description = "disable the converting feature completely; if you want to only append the channel links")
+    private boolean noConverter;
+
+    @Option(names = {"-c", "--channel", "--channel-link", "--create-channels", "--create-channel-links"},
+            description = "enable the appending feature")
+    private boolean doChannelLinks;
+
+
+    @Option(names = {"-i", "--in", "--json"},
+            defaultValue = "org.eclipse.smarthome.core.items.Item.json",
+            description = "specify the input .json file")
+    private String jsonFile;
+
+    @Option(names = {"--channel-file", "--channel-link-file"},
+            defaultValue = "org.eclipse.smarthome.core.thing.link.ItemChannelLink.json",
+            description = "specify the .json file location containing the channel links")
+    private String channelFile;
+
+    @Option(names = {"-o", "--out", "--items"},
+            defaultValue = "json.items",
+            description = "specify the output file")
+    private String outFile;
+
+    @Option(names = {"-d", "--dir", "--directory"},
+            defaultValue = ".",
+            description = "specify the directory in which to search for *.items files")
+    private String directory;
 
 
     public static void main(String[] args) {
-        // args evaluating:
-        boolean inNext = false;
-        boolean chNext = false;
-        boolean outNext = false;
-        boolean dirNext = false;
+        new CommandLine(new Starter()).execute(args);
+    }
 
-        boolean printHelp = false;
-        boolean doConverter = true; // default true
-        boolean doChannelLinks = false;
 
-        String jsonFile = DEFAULT_JSONFILE;
-        String channelFile = DEFAULT_CHANNELFILE;
-        String outFile = DEFAULT_OUTFILE;
-        String directory = DEFAULT_DIRECTORY;
-
-        loop:
-        for (int i = 0; i < args.length; i++) {
-            if (inNext) {
-                jsonFile = args[i];
-                log.info("Using jsonFile={}", jsonFile);
-                inNext = false;
-            } else if (chNext) {
-                channelFile = args[i];
-                log.info("Using channelFile={}", channelFile);
-                chNext = false;
-            } else if (outNext) {
-                outFile = args[i];
-                log.info("Using outFile={}", outFile);
-                outNext = false;
-            } else if (dirNext) {
-                directory = args[i];
-                log.info("Using directory={}", directory);
-                dirNext = false;
-            } else {
-                switch (args[i]) {
-                    case "-i":
-                    case "--in":
-                    case "--json":
-                        if (args.length >= i + 2) {
-                            inNext = true;
-                        }
-                        break;
-
-                    case "--channel-file":
-                    case "--channel-link-file":
-                        if (args.length >= i + 2) {
-                            chNext = true;
-                        }
-                        break;
-
-                    case "-o":
-                    case "--out":
-                    case "--items":
-                        if (args.length >= i + 2) {
-                            outNext = true;
-                        }
-                        break;
-
-                    case "-d":
-                    case "--dir":
-                    case "--directory":
-                        if (args.length >= i + 2) {
-                            dirNext = true;
-                        }
-                        break;
-
-                    case "-c":
-                    case "--channel":
-                    case "--channel-link":
-                    case "--create-channels":
-                    case "--create-channel-links":
-                        doChannelLinks = true;
-                        break;
-
-                    case "--no-items":
-                    case "-n":
-                        doConverter = false;
-                        break;
-
-                    default:
-                        printHelp = true;
-                        break loop;
-                }
-            }
-        }
-        if (printHelp) {
-            log.warn("Wrong parameter usage");
-            System.out.println(
-                    "Usage: json2config-XXX.jar [--in <path>] [--out <path>] [--no-items] [--create-channel-links] [--directory] [--channel-file]");
-            return;
-        }
-
+    /**
+     * Starts execution after args are evaluated.
+     */
+    @Override
+    public void run() {
         // start Converter:
-        if (doConverter) {
+        if (!noConverter) {
             Type type = Type.ITEM;
             log.info("Starting Converter with jsonFile={}, outFile={}, type={}",
                     jsonFile, outFile, type);
