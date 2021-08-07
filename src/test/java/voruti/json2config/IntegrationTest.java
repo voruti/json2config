e4a030002558d6c97f61a67e53854dc8d37b600d
voruti <45130096+voruti@users.noreply.github.com>
2021-08-07 20:56:02 +0200

    Create test for multiple channels

--- src/test/java/voruti/json2config/IntegrationTest.java
+++ src/test/java/voruti/json2config/IntegrationTest.java
@@ -1,6 +1,7 @@
 package voruti.json2config;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,16 +15,22 @@ class IntegrationTest {
     private static final String RESOURCES = "build/resources/test/";
     private static final String TEMPORARY = "build/tmp/test/";
 
-    @Test
-    void openhab2_example1() throws IOException {
+
+    private String openFile(String file) throws IOException {
+        Path path = Paths.get(file);
+        return String.join("\n", Files.readAllLines(path))
+                .replaceAll("[\\h\\t ]{2,}", " ");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"openhab2_example1", "openhab2_multipleChannelsOneItem"})
+    void itemsAndChannels(String testName) throws IOException {
         // arrange:
-        final String testName = "openhab2_example1";
         final String generatedItemsFile = TEMPORARY + testName + "_result.items";
 
         // load template .items file:
-        Path pathTemplate = Paths.get(RESOURCES + testName + ".items");
-        String template = String.join("\n", Files.readAllLines(pathTemplate))
-                .replaceAll("[\\h\\t ]{2,}", " ");
+        String template = openFile(RESOURCES + testName + ".items");
 
         // act:
         Starter.main(new String[]{"-i", RESOURCES + testName + ".Item.json",
@@ -34,23 +41,19 @@ class IntegrationTest {
 
         // assert:
         // load generated .items file:
-        Path pathGenerated = Paths.get(generatedItemsFile);
-        String generated = String.join("\n", Files.readAllLines(pathGenerated))
-                .replaceAll("[\\h\\t ]{2,}", " ");
+        String generated = openFile(generatedItemsFile);
 
         assertEquals(template, generated);
     }
 
-    @Test
-    void openhab2_exampleDimensions() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = {"openhab2_exampleDimensions"})
+    void onlyItems(String testName) throws IOException {
         // arrange:
-        final String testName = "openhab2_exampleDimensions";
         final String generatedItemsFile = TEMPORARY + testName + "_result.items";
 
         // load template .items file:
-        Path pathTemplate = Paths.get(RESOURCES + testName + ".items");
-        String template = String.join("\n", Files.readAllLines(pathTemplate))
-                .replaceAll("[\\h\\t ]{2,}", " ");
+        String template = openFile(RESOURCES + testName + ".items");
 
         // act:
         Starter.main(new String[]{"-i", RESOURCES + testName + ".Item.json",
@@ -58,9 +61,7 @@ class IntegrationTest {
 
         // assert:
         // load generated .items file:
-        Path pathGenerated = Paths.get(generatedItemsFile);
-        String generated = String.join("\n", Files.readAllLines(pathGenerated))
-                .replaceAll("[\\h\\t ]{2,}", " ");
+        String generated = openFile(generatedItemsFile);
 
         assertEquals(template, generated);
     }
