309a18337a9a91577e657edbf92adf13a29d66be
voruti <45130096+voruti@users.noreply.github.com>
2021-08-11 21:41:21 +0200

    Add v3 argument

--- src/main/java/voruti/json2config/service/Constants.java
+++ src/main/java/voruti/json2config/service/Constants.java
@@ -2,7 +2,14 @@ package voruti.json2config.service;
 
 public final class Constants {
 
+    // logging constants:
     public static final String LOG_CANT_OPEN_FILE = "Can't open file {}";
+
+    // default voruti.json2config.Starter argument values:
+    public static final String DEFAULT_V2_JSON_FILE = "org.eclipse.smarthome.core.items.Item.json";
+    public static final String DEFAULT_V3_JSON_FILE = "org.openhab.core.items.Item.json";
+    public static final String DEFAULT_V2_CHANNEL_FILE = "org.eclipse.smarthome.core.thing.link.ItemChannelLink.json";
+    public static final String DEFAULT_V3_CHANNEL_FILE = "org.openhab.core.thing.link.ItemChannelLink.json";
 
 
     private Constants() {
